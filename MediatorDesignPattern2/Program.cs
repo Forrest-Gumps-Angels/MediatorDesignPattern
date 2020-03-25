eade7f7de18c10bfa6a2cbe7b998c6c86de0cb9b
Capriskrald <hansenmann2497@hotmail.com>
2020-03-25 11:32:06 +0100

    added project2

---
+++ MediatorDesignPattern2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MediatorDesignPattern2
+{
+    public interface IMediator
+    {
+        void Notify(object sender, string msg);
+    }
+
+    public class Mediator : IMediator
+    {
+        public Mediator()
+        {
+
+        }
+
+        public void Notify(object sender, string msg)
+        {
+
+        }
+    }
+
+    public class BaseComponent 
+    {
+        public BaseComponent(IMediator m)
+        {
+            _mediator = m;
+        }
+
+        protected IMediator _mediator;
+    }
+
+    public class ComponentA : BaseComponent
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("ComponentA did something");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
